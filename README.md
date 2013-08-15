f531c562096e38cdb5f69915fa11565daf400bf3
Mark Dawson <mark@clipboard.com>
2013-08-14 19:54:20 -0700

    v 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 MoreThanAnImage
 ===============
 
-Retrieve images and image header information via JavaScript
+#Overview
+Sometimes you want more than just an image, potentially an image plus some associated metadata, rather than
+having to call an API to fetch the metadata embed it directly in HTTP headers associated with the image. By
+doing this you can immediately show information without having to wait for API calls, plus if you are hosting
+images on a CDN or some other service, you can eliminate having the overhead of managing the extra APIs.
+
+##Example usage
+```javascript
+    var url = '/google.jpg';
+    var loadRequest = MoreThanAnImage.load(url, function(error, response) {
+        if(error) {
+            alert('got an error:' + error.status);
+            return;
+        }
+        $('#imageContainer').append(response.image);
+
+        var htmlString = '';
+        for(var key in response.headers) {
+            htmlString += key + ' => ' + response.headers[key] + '<br />';
+        }
+        $('#headerContainer').append('<p>' + htmlString + '</p>');
+    });
+```
+
+##Demo
+Click on the <a href="http://markdaws.github.io/MoreThanAnImage">link</a> to see a live demo, an image should load and show the associated HTTP headers.
+
+##Cross domain
+Since the code uses an XmlHttpRequest object, you will need to setup the CORS response on your server. For example,
+here is how you setup CORS on <a href="http://docs.aws.amazon.com/AmazonS3/latest/dev/cors.html">Amazon</a>.
+
+NOTE: CORS is supported by IE10 and up, this won;t work on IE9, if anyone knows a workaround (yes I tried
+XDomainRequest, but it doesn't support binary content only text "face palm".
+
+##Future TODOs:
+1. Support EXIF parsing
+2. Add download progress information
+
+##Development
+You don't need to install npm (npmjs.org) but you can use it to host a mini webserver for development:
+
+```bash
+git clone git@github.com:markdaws/MoreThanAnImage.git
+cd MoreThanAnImage
+npm install
+npm start
+
+open http://localhost:9999/demo/index.html
+```
--- more-than-an-image.js
+++ more-than-an-image.js
@@ -1,99 +1,129 @@
-$(document).ready(function() {
+;(function(context) {
 
+    var MoreThanAnImage = context.MoreThanAnImage = function() {
+    };
 
-    function notLame() {
+    MoreThanAnImage.load = function(url, callback) {
+        var supportsResponseType = typeof new XMLHttpRequest().responseType === 'string';
+        if (supportsResponseType) {
+            loadViaBlob(url, function(error, response) {
+                callback(error, response);
+            });
+        }
+        else {
+            loadForIE9(url, function(error, response) {
+                callback(error, response);
+            });
+        }
+    };
+
+    function parseHeaders(xhr) {
+        var parsedHeaders = {},
+            headerString = xhr.getAllResponseHeaders(),
+            headers = headerString.split('\n');
+        for (var i=0; i<headers.length; ++i) {
+            if (/^\s*$/.test(headers[i])) {
+                continue;
+            }
+            var keyValue = headers[i].split(': ');
+            parsedHeaders[keyValue[0].toLowerCase()] = keyValue[1];
+        }
+        return parsedHeaders;
+    }
+
+    function loadViaBlob(url, callback) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", 'dog.jpeg', true);
-        xhr.responseType = "blob";
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == xhr.DONE) {
-                var blob = xhr.response;
-                //var image = //document.getElementById("my-image");
-                
-                var image = document.createElement('img');
-                $('body').append(image);
-                image.addEventListener("load", function (evt) {
-                    URL.revokeObjectURL(evt.target.src);
+        xhr.responseType = 'blob';
+        xhr.onload = function(e) {
+            if (xhr.status !== 200) {
+                callback({ status: xhr.status });
+                return;
+            }
+            
+            var blob = xhr.response;
+            var image = document.createElement('img');
+            image.addEventListener('load', function (evt) {
+                URL.revokeObjectURL(evt.target.src);
+
+                callback(null, {
+                    image: image,
+                    headers: parseHeaders(xhr)
                 });
-                image.src = URL.createObjectURL(blob);
-                
-                console.log(xhr.getResponseHeader('etag'));
-            }
+            });
+            image.src = URL.createObjectURL(blob);
         };
+
+        xhr.onerror = function() {
+            callback({});
+            console.log('on error');
+        };
+        xhr.open('GET', url, true);
         xhr.send();
     }
 
-    function lame() {
+    function loadForIE9(url, callback) {
 
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", 'dog.jpeg', true);
-        //xhr.responseType = "blob";
-        xhr.overrideMimeType('text/plain; charset=x-user-defined'); 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == xhr.DONE) {
-                var image = document.createElement('img');
-
-                console.time('encode');
-                image.src="data:imagejpeg;base64," + encode64(xhr.responseText);
-                console.timeEnd('encode');
-                $('body').append(image);
-                console.log(xhr.getResponseHeader('etag'));
+        xhr.onload = function(e) {
+            if (xhr.status !== 200) {
+                callback({ status: xhr.status });
+                return;
             }
-        };
-        xhr.send();
+            
+            var image = document.createElement('img');
+            image.addEventListener('load', function (evt) {
+                callback(null, {
+                    image: image,
+                    headers: parseHeaders(xhr)
+                });
+            });
 
-        
+            // From: http://emilsblog.lerch.org/2009/07/javascript-hacks-using-xhr-to-load.html
+            var data = new VBArray(xhr.responseBody).toArray();
+            image.src='data:imagejpeg;base64,' + encode64Array(data);
+        };
+
+        xhr.onerror = function() {
+            callback({});
+            console.log('on error');
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
     }
 
-    //TODO: Fallback for no blob support ...
+    // From: http://www.philten.com/us-xmlhttprequest-image/
+    function encode64Array(input) {
+        var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+            outputStr = '',
+            i = 0;
+        
+        while (i<input.length) {
+            //all three "& 0xff" added below are there to fix a known bug 
+            //with bytes returned by xhr.responseText
+            var byte1 = input[i++] & 0xff;
+            var byte2 = input[i++] & 0xff;
+            var byte3 = input[i++] & 0xff;
 
-    lame();
+            var enc1 = byte1 >> 2;
+            var enc2 = ((byte1 & 3) << 4) | (byte2 >> 4);
+            
+            var enc3, enc4;
+            if (isNaN(byte2)) {
+                enc3 = enc4 = 64;
+            }
+            else {
+                enc3 = ((byte2 & 15) << 2) | (byte3 >> 6);
+                if (isNaN(byte3)) {
+                    enc4 = 64;
+                }
+                else {
+                    enc4 = byte3 & 63;
+                }
+            }
+            outputStr += b64.charAt(enc1) + b64.charAt(enc2) + b64.charAt(enc3) + b64.charAt(enc4);
+        }
+        return outputStr;
+    }
 
-
-// Fallback code from: http://www.philten.com/us-xmlhttprequest-image/
-
-function encode64(inputStr) 
-{
-   var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-   var outputStr = "";
-   var i = 0;
-   
-   while (i<inputStr.length)
-   {
-      //all three "& 0xff" added below are there to fix a known bug 
-      //with bytes returned by xhr.responseText
-      var byte1 = inputStr.charCodeAt(i++) & 0xff;
-      var byte2 = inputStr.charCodeAt(i++) & 0xff;
-      var byte3 = inputStr.charCodeAt(i++) & 0xff;
-
-      var enc1 = byte1 >> 2;
-      var enc2 = ((byte1 & 3) << 4) | (byte2 >> 4);
-         
-         var enc3, enc4;
-         if (isNaN(byte2))
-                {
-                    enc3 = enc4 = 64;
-                       }
-         else
-               {
-                   enc3 = ((byte2 & 15) << 2) | (byte3 >> 6);
-                   if (isNaN(byte3))
-                         {
-           enc4 = 64;
-                               }
-                   else
-                         {
-                             enc4 = byte3 & 63;
-                               }
-                     }
-
-      outputStr += b64.charAt(enc1) + b64.charAt(enc2) + b64.charAt(enc3) + b64.charAt(enc4);
-   } 
-   
-   return outputStr;
-}
-
-
-
-
-});
+})(window);
